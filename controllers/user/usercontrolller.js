13581456d39e30c6d92873c608b42e78a2ce197a
Tony fernandez <fernandeztony807@gmail.com>
2024-09-12 08:26:45 +0530

    Versioning untracked files

---
+++ controllers/user/usercontrolller.js
@@ -0,0 +1,19 @@
+
+
+
+const loadHompepage = async (req,res)=>{
+    try {
+        return res.render("home");
+
+    } catch (error) {
+        console.log("Home page not found");
+        res.status(500).send("Server error")
+
+    }
+    
+}
+
+
+module.exports = {
+    loadHompepage,
+}
